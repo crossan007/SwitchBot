e839b1ea0fd9402f512d19357ac35e39724f6366
crossan007 <crossan007@gmail.com>
2015-11-01 18:39:02 -0500

    WebPowerSwitch
    Deal with the awful HTML DOM on these things.  Scrape the UI into some
    useful variables, and parse outlet names and power states.

--- plugins/webpowerswitch/__init__.py
+++ plugins/webpowerswitch/__init__.py
@@ -1,3 +1,8 @@
+from lxml import html
+import requests
+import re
+
+
 def run(self):
     print("Hello from the Web Power Switch Plugin!")
     
@@ -8,3 +13,17 @@ def getControlObjectClassGUID():
 def getControlObjectProperties():
     return "Some outlets"
     
+def getControlPage(IP,username,password):
+    """
+    Get the WebPowerSwitch's control page
+    """
+    page = requests.get("http://192.168.10.53/index.htm",auth=('admin','HaveFaith2'))
+    tree = html.fromstring(page.text)
+    outletNumbers =  tree.xpath('/html/body/font/table/tr/td[2]/table[2]/tr[position()>2]/td[1]/text()')
+    outletNames =  tree.xpath('/html/body/font/table/tr/td[2]/table[2]/tr[position()>2]/td[2]/text()')
+    outletStates =  tree.xpath('/html/body/font/table/tr/td[2]/table[2]/tr[position()>2]/td[3]/b/font/text()')
+
+
+
+def getSetupPage(IP,username,password):
+
